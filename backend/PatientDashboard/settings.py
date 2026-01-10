71c0f3ac40618ee58762e1d3d85440954f65ce79
Eng_Omar_Mohamed <42245792+omarmohammed271@users.noreply.github.com>
2026-01-10 10:45:53 +0200

    Update CORS_ALLOWED_ORIGINS for IP address

--- backend/PatientDashboard/settings.py
+++ backend/PatientDashboard/settings.py
@@ -67,7 +67,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://localhost:3000",
-    "https://87.237.225.79:3000",
+    "http://87.237.225.79:3000",
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
