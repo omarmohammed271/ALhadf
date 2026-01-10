997f92c1161047ca00d6ddb2af0545c13dc920b1
Saif Ayman <saifayman3021@gmail.com>
2026-01-10 17:14:21 +0200

    Update axiosInstance.ts

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -5,7 +5,7 @@ import axios from 'axios';
 export const baseURL = `87.237.225.79:8000`
 
 export const axiosInstance = axios.create({
-  baseURL: `https://${baseURL}/`,
+  baseURL: `http://${baseURL}/`,
   headers: {
     'Content-Type': 'application/json',
     'Authorization': `Token ${localStorage.getItem('user-token')}`
@@ -13,14 +13,14 @@ export const axiosInstance = axios.create({
 });
 
 export const axiosAuthInstance = axios.create({
-  baseURL: `https://${baseURL}/`,
+  baseURL: `http://${baseURL}/`,
   headers: {
     'Content-Type': 'multipart/form-data',
   },
 });
 
 export const axiosMutateInstance = axios.create({
-    baseURL: `https://${baseURL}/`,
+    baseURL: `http://${baseURL}/`,
     headers: {
       'Content-Type': 'multipart/form-data',
       'Authorization': `Token ${localStorage.getItem('user-token')}`
